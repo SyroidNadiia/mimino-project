8556c135526392620204f121a47accd1a08d41a6
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2022-12-11 15:31:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './js/imask';
 import './js/nice-select2';
 import './js/modal';
 import './js/modalinputs';
-import './js/hotel-details__swiper';
 import './js/swiper-config';
 import './js/scroll-up';
 
